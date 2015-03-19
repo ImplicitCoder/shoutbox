d02c726ba06827d4b113663190409ef6f1628579
debaze <joris.derese@gmail.com>
2015-03-19 21:50:11 +0100

    v 0.0.1 ready

--- package.js
+++ package.js
@@ -14,14 +14,14 @@ Package.onUse(function(api) {
   api.use([
       'session',
       'templating',
-      'momentjs:moment',
-      'mrt:jquery-ui',
-      'sacha:spin',
-      'twbs:bootstrap',
+      'momentjs:moment@2.9.0',
+      'mrt:jquery-ui@1.9.2',
+      'sacha:spin@2.0.4',
+      'twbs:bootstrap@3.3.2',
       'mongo',
       'livedata'
   ]);
-
+ api.versionsFrom('METEOR@1.0.4.1');
   api.add_files('client/template/styles/subscriptions.js', 'client');
   api.add_files('client/template/shoutbox.html','client');
   api.add_files('lib/collections/shouts.js', both);
